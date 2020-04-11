d254e729d3a62a8bc37a0cf48220262700fe6e11
Krystian Carrillo <krycar12@gmail.com>
2020-04-10 20:32:45 -0600

    Final Push

--- java/src/newSorting.java
+++ java/src/newSorting.java
@@ -1,2 +1,83 @@
-public class newSorting {
+public class newSorting{
+    public void newSorting(int[] arry, int size){
+        if(arry.length <= size)
+            quickSort(arry, 0, arry.length-1);
+
+        else{
+            int[] left = new int[arry.length / 2];
+            int[] right = new int[arry.length - left.length];
+
+            for(int i=0; i<left.length; i++){
+                left[i] = arry[i];
+            }
+            newSorting(left, size);
+
+            for(int i=0; i<right.length; i++){
+                right[i] = arry[i + left.length];
+            }
+            newSorting(right, size);
+
+            mergeSortedHalves(arry, left, right);
+        }
+    }
+
+    private void mergeSortedHalves(int[] arry, int[] left, int[] right){
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while(index < arry.length){
+
+            if(i < left.length && j < right.length){
+                if(left[i] < right[j]){
+                    arry[index] = left[i];
+                    i++;
+                }
+
+                else{
+                    arry[index] = right[j];
+                    j++;
+                }
+            }
+
+            else{
+                arry[index] = left[i];
+                i++;
+            }
+            index++;
+        }
+
+        if(j < right.length){
+            arry[index] = right[j];
+            j++;
+            index++;
+        }
+    }
+
+    private void quickSort(int[] arry, int start, int end){
+        if(start < end){
+            int index = partition(arry, start, end);
+            quickSort(arry, start, index - 1);
+            quickSort(arry, index + 1, end);
+        }
+    }
+
+    private int partition(int[] arry, int start, int end){
+        int pivot = arry[start];
+        int i = start;
+        int temp;
+
+        for(int j=start+1; j<=end; j++){
+            if(arry[j] < pivot){
+                i++;
+                temp = arry[i];
+                arry[i] = arry[j];
+                arry[j] = temp;
+            }
+        }
+        
+        temp = arry[start];
+        arry[start] = arry[i];
+        arry[i] = temp;
+        return i;
+    }
 }
--- java/src/newSortingTester.java
+++ java/src/newSortingTester.java
@@ -1,2 +1,59 @@
-public class newSortingTester {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class newSortingTester{
+    //This test case tests to see if newSorting.java works with an array of all positive numbers
+    //Passed
+    @Test
+    public void newSorting1(){
+        int[] arry = {10, 4, 7, 1, 5, 2, 8, 9, 3, 6};
+        int[] testArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        newSorting sol = new newSorting();
+        sol.newSorting(arry,10);
+        assertArrayEquals(testArr, arry);
+    }
+
+    //This test case tests to see if newSorting.java works with an array of negative and positive numbers
+    //Passed
+    @Test
+    public void newSorting2(){
+        int[] arry = {-4, 0, -3, -7, 9, 2, 4, 8, -9, 1};
+        int[] testArr = {-9, -7, -4, -3, 0, 1, 2, 4, 8, 9};
+        newSorting sol = new newSorting();
+        sol.newSorting(arry,10);
+        assertArrayEquals(testArr, arry);
+    }
+
+    //This test case tests to see if newSorting.java works with an array that has duplicate numbers
+    //Passed
+    @Test
+    public void newSorting3(){
+        int[] arry = {0, 2, 7, 1, 10, 10, 1, 2, 1, 4};
+        int[] testArr = {0, 1, 1, 1, 2, 2, 4, 7, 10, 10};
+        newSorting sol = new newSorting();
+        sol.newSorting(arry,10);
+        assertArrayEquals(testArr, arry);
+    }
+
+    //This test case tests to see if newSorting.java works with an array of all the same numbers
+    //Passed
+    @Test
+    public void newSorting4(){
+        int[] arry = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        int[] testArr = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        newSorting sol = new newSorting();
+        sol.newSorting(arry,10);
+        assertArrayEquals(testArr, arry);
+    }
+
+    //This test case tests to see if newSorting.java works with already sorted array of numbers
+    //Passed
+    @Test
+    public void newSorting5(){
+        int[] arry = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] testArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        newSorting sol = new newSorting();
+        sol.newSorting(arry,10);
+        assertArrayEquals(testArr, arry);
+    }
 }
